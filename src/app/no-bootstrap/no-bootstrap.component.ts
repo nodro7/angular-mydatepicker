b1ec62212fe9c318cde1540b298e86488364ec18
kekeh <kea789@outlook.com>
2019-05-10 18:09:16 +0300

    Initial version.

--- src/app/no-bootstrap/no-bootstrap.component.ts
+++ src/app/no-bootstrap/no-bootstrap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {IAngularMyDpOptions, IMyDateModel} from 'angular-mydatepicker';
+
 @Component({
   selector: 'app-no-bootstrap',
   templateUrl: './no-bootstrap.component.html',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoBootstrapComponent implements OnInit {
 
+  private myOptions: IAngularMyDpOptions = {
+    dateRange: false,
+    dateFormat: 'dd.mm.yyyy'
+  };
+
+  private model: IMyDateModel = null;
+
   constructor() { }
+
+  // optional date changed callback
+  onDateChanged(event: IMyDateModel): void {
+    console.log('onDateChanged(): ', event);
+  }
 
   ngOnInit() {
   }
