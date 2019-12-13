e4b5142ce01cbde5a3c22f0c06df14e8cb8fd443
kekeh <kea789@outlook.com>
2019-12-13 09:37:54 +0200

    Demo page app updated. Angular 9-rc6.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -28,7 +28,7 @@ import 'core-js/es6/reflect';
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+//import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -63,3 +63,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
