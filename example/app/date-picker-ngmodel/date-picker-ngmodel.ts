00692b34a0429155ca8900524a52b63733219a2e
kekeh <kea789@outlook.com>
2020-02-21 12:53:03 +0200

    Example app updated.

--- example/app/date-picker-ngmodel/date-picker-ngmodel.ts
+++ example/app/date-picker-ngmodel/date-picker-ngmodel.ts
@@ -294,63 +294,6 @@ export class DatePickerNgmodel implements OnInit {
     this.myDatePickerOptions = copy;
   }
 
-  onOverrideStyles(checked: boolean): void {
-    let copy = this.getCopyOfOptions();
-
-    copy.stylesData = checked ?
-      {
-        selector: 'dp1',
-        styles: `
-        .dp1 .myDpIconLeftArrow,
-        .dp1 .myDpIconRightArrow,
-        .dp1 .myDpHeaderBtn {
-            color: #3855c1;
-        }
-        .dp1 .myDpHeaderBtn:focus,
-        .dp1 .myDpMonthLabel:focus,
-        .dp1 .myDpYearLabel:focus {
-          color: #66afe9;
-        }
-        .dp1 .myDpCurrMonth,
-        .dp1 .myDpMonthcell,
-        .dp1 .myDpYearcell {
-            color: #3855c1;
-        }
-        .dp1 .myDpPrevMonth,
-        .dp1 .myDpNextMonth {
-            color: #5f9ea0;
-        }
-        .dp1 .myDpWeekDayTitle {
-            background-color: #5289ec;
-            color: #ffffff;
-        }
-        .dp1 .myDpHeaderBtnEnabled:hover,
-        .dp1 .myDpMonthLabel:hover,
-        .dp1 .myDpYearLabel:hover {
-            color: #add8e6;
-        }
-        .dp1 .myDpMarkCurrDay, 
-        .dp1 .myDpMarkCurrMonth, 
-        .dp1 .myDpMarkCurrYear {
-            border-bottom: 2px solid #3855c1;
-        }
-        .dp1 .myDpDisabled {
-            color: #999;
-        }
-        .dp1 .myDpHighlight {
-            color: #cd5c5c;
-        }
-        .dp1 .myDpTableSingleDay:hover, 
-        .dp1 .myDpTableSingleMonth:hover, 
-        .dp1 .myDpTableSingleYear:hover {
-            background-color: #add8e6;
-        }
-      `
-      } : {selector: '', styles: ''}
-
-    this.myDatePickerOptions = copy;
-  }
-
   onDisableUntilYesterday(checked: boolean) {
     let copy = this.getCopyOfOptions();
     let d: Date = new Date();
