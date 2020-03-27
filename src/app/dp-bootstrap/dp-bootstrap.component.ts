80044db3674f3dc7c7c7f232961c75c88b92abac
kekeh <kea789@outlook.com>
2020-03-27 17:30:17 +0200

    Demo page app updated.

--- src/app/dp-bootstrap/dp-bootstrap.component.ts
+++ src/app/dp-bootstrap/dp-bootstrap.component.ts
@@ -11,7 +11,21 @@ export class DpBootstrapComponent implements OnInit {
 
   public myOptions: IAngularMyDpOptions = {
     dateRange: false,
-    dateFormat: 'dd.mm.yyyy'
+    dateFormat: 'd. of mmm yyyy',
+    monthLabels: { 
+      1: 'January', 
+      2: 'February', 
+      3: 'March', 
+      4: 'April', 
+      5: 'May', 
+      6: 'June', 
+      7: 'July', 
+      8: 'August', 
+      9: 'September', 
+      10: 'October', 
+      11: 'November', 
+      12: 'December' 
+    }
   };
 
   public model: IMyDateModel = null;
--- src/app/drp-bootstrap/drp-bootstrap.component.ts
+++ src/app/drp-bootstrap/drp-bootstrap.component.ts
@@ -12,7 +12,21 @@ export class DrpBootstrapComponent implements OnInit {
   public myOptions: IAngularMyDpOptions = {
     dateRange: true,
     dateFormat: 'dd.mm.yyyy',
-    calendarAnimation: {in: CalAnimation.FlipDiagonal, out: CalAnimation.Rotate}
+    calendarAnimation: {in: CalAnimation.FlipDiagonal, out: CalAnimation.Rotate},
+    monthLabels: { 
+      1: 'January', 
+      2: 'February', 
+      3: 'March', 
+      4: 'April', 
+      5: 'May', 
+      6: 'June', 
+      7: 'July', 
+      8: 'August', 
+      9: 'September', 
+      10: 'October', 
+      11: 'November', 
+      12: 'December' 
+    }
   };
 
   public model: IMyDateModel = null;
