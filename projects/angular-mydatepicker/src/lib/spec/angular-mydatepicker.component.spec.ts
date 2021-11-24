e437510794fd35358b157ebe4b1e477315520839
Gordon Boyle <gordonboyle@gmail.com>
2021-11-24 10:57:23 +0000

    feat: update to angular 11

--- projects/angular-mydatepicker/src/lib/spec/angular-mydatepicker.component.spec.ts
+++ projects/angular-mydatepicker/src/lib/spec/angular-mydatepicker.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {FormsModule} from '@angular/forms';
 import {By} from '@angular/platform-browser';
@@ -87,7 +87,7 @@ class AngularMyDatepickerTestComponent {
 }
 
 describe('AngularMyDatePickerComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AngularMyDatepickerTestComponent],
       imports: [FormsModule, AngularMyDatePickerModule],
